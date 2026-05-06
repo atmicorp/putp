a41bfcea7f4f9c3d3bdc79c354ce02882c0d9b44
Yulius Caesar <151473638+YuliusCaesar01@users.noreply.github.com>
2026-05-06 16:56:16 +0700

    update route

--- Modules/User/routes/web.php
+++ Modules/User/routes/web.php
@@ -21,5 +21,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return response()->file(
             Storage::disk('private')->path($user->signature_path)
         );
-    })->name('signature.show');
+    })->name('customer.signature.show');
 });
